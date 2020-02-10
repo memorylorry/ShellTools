8c53b900be36a5a75ae8eea47131a65d29f25662
memorylorry <294092869@qq.com>
2020-02-10 18:55:15 +0800

    Update install.sh

--- shadowsock/install.sh
+++ shadowsock/install.sh
@@ -5,8 +5,6 @@ PASS=9527
 # 加密方法
 METHOD="aes-256-cfb"
 
-
-
 # 下载pip 并 安装
 curl --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
 
@@ -14,3 +12,7 @@ curl --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python
 pip install shadowsocks
 
 ssserver -p $PORT -k $PASS -m $METHOD --workers 10 --pid-file /tmp/ss.pid --log-file /tmp/ss.log -d start
+
+echo "端口： "$PORT
+echo "密码： "$PORT
+echo "加密方式： "$METHOD
